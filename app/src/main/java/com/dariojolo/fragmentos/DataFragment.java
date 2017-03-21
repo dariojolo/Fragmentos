eeb29f225ab453fadcd07d55c41779852f97b846
dariojolo <dariojolo@gmail.com>
2017-03-20 22:52:50 -0300

    Initial commit

---
+++ app/src/main/java/com/dariojolo/fragmentos/DataFragment.java
@@ -0,0 +1,34 @@
+package com.dariojolo.fragmentos;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DataFragment extends Fragment {
+
+    private EditText txtIngreso;
+    private Button  btnSend;
+
+    public DataFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_data,container, false);
+        // Inflate the layout for this fragment
+        return view;
+    }
+
+}
